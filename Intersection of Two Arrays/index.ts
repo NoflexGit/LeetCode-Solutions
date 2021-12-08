6f1d83f930f36669ffe82ffc8a3db175faa9f492
NoflexGit <untroman@mail.ru>
2021-12-08 07:04:11 +0300

    Refactoring

--- Intersection of Two Arrays/index.ts
+++ Intersection of Two Arrays/index.ts
@@ -1,12 +1,11 @@
 export const intersectionOfArrays = (arr1: Array<number>, arr2: Array<number>): Array<number> => {
   arr1 = [...new Set(arr1)];
   arr2 = [...new Set(arr2)];
-  const result = arr1
+  return arr1
     .filter((i) => arr2.includes(i))
     .sort(function (a, b) {
       return a - b;
     });
-  return result;
 };
 
 export default intersectionOfArrays;
